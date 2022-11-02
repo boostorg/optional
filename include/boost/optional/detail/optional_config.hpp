168552e021d97807a299996c05c7ac985a954389
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-11-01 22:02:03 -0400

    spelling: msvc
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- include/boost/optional/detail/optional_config.hpp
+++ include/boost/optional/detail/optional_config.hpp
@@ -53,7 +53,7 @@
 #endif
 
 #if (defined(_MSC_VER) && _MSC_VER <= 1800)
-// on MSCV 2013 and earlier an unwanted temporary is created when you assign from
+// on MSVC 2013 and earlier an unwanted temporary is created when you assign from
 // a const lvalue of integral type. Thus we bind not to the original address but
 // to a temporary. 
 # define BOOST_OPTIONAL_CONFIG_NO_PROPER_ASSIGN_FROM_CONST_INT
